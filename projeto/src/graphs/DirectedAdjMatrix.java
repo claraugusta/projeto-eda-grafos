f39e15f93f9c1390156995d01bce6c40b99d0f2f
claraugusta <mc.augusta1052@gmail.com>
2025-02-18 15:03:54 -0300

    matriz de adjacencia direcionada

--- projeto/src/graphs/DirectedAdjMatrix.java
+++ projeto/src/graphs/DirectedAdjMatrix.java
@@ -2,8 +2,8 @@ package graphs;
 
 import java.util.Arrays;
 
-//grafo nao direcionado e ponderado
-public class AdjMatrix {
+//grafo direcionado e ponderado
+public class DirectedAdjMatrix {
 
     private int nullEdgeValue;
     private int maxNodes;
@@ -11,7 +11,7 @@ public class AdjMatrix {
     private int[] nodes;
     private int[][] adjacencyMatrix;
 
-    public AdjMatrix(int maxNodes, int nullEdgeValue){
+    public DirectedAdjMatrix(int maxNodes, int nullEdgeValue){
         this.maxNodes = maxNodes;
         this.nullEdgeValue = nullEdgeValue;
         this.qtdNodes = 0;
@@ -44,8 +44,6 @@ public class AdjMatrix {
         int c = getNodeIndex(nodeIn);
         if(l == -1 || c == -1)
             return false;
-        //funciona pra grafos nao direcionados
-        this.adjacencyMatrix[l][c] = weigth;
         this.adjacencyMatrix[c][l] = weigth;
         return true;
     }
@@ -58,18 +56,29 @@ public class AdjMatrix {
         return this.adjacencyMatrix[l][c];
     }
 
-    public int getDegree(int node){
+    public int getOutDegree(int node){
         int l = getNodeIndex(node);
         if(l == -1)
             return -1;
-        int degree = 0;
+        int outDegree = 0;
         for (int i = 0; i < maxNodes; i++) {
             if(this.adjacencyMatrix[l][i] != this.nullEdgeValue)
-                degree++;
+                outDegree++;
         }
-        return degree;
+        return outDegree;
     }
 
+    public int getInDegree(int node){
+        int c = getNodeIndex(node);
+        if(c == -1)
+            return -1;
+        int inDegree = 0;
+        for (int i = 0; i < maxNodes; i++) {
+            if(this.adjacencyMatrix[i][c] != this.nullEdgeValue)
+                inDegree++;
+        }
+        return inDegree;
+    }
     public String MatrixToString(){
         String out = "";
         for (int i = 0; i < this.maxNodes; i++) {
@@ -88,6 +97,41 @@ public class AdjMatrix {
             out += "pos: " + i + ", node: " + this.nodes[i] + "\n";
         }
         return out;
+    }
+
+    public static void main(String[] args) {
+        // Criando um grafo com capacidade para 5 nós e valor nulo como 0
+        DirectedAdjMatrix graph = new DirectedAdjMatrix(5, 0);
+
+        // Adicionando nós
+        graph.addNode(10);
+        graph.addNode(20);
+        graph.addNode(30);
+        graph.addNode(40);
+        graph.addNode(50);
+
+        // Adicionando arestas com pesos
+        graph.addEdge(10, 20, 5);
+        graph.addEdge(10, 30, 2);
+        graph.addEdge(20, 40, 8);
+        graph.addEdge(30, 50, 3);
+        graph.addEdge(40, 50, 7);
+
+        // Exibindo a matriz de adjacência
+        System.out.println("Matriz de Adjacência:");
+        System.out.println(graph.MatrixToString());
+
+        // Exibindo os nós armazenados
+        System.out.println("Nós do Grafo:");
+        System.out.println(graph.nodesToString());
+
+        // Testando a recuperação de peso
+        System.out.println("Peso da aresta entre 10 e 30: " + graph.getWeight(10, 30));
+        System.out.println("Peso da aresta entre 20 e 40: " + graph.getWeight(20, 40));
+
+        // Testando o grau dos nós
+        System.out.println("Grau do nó 10: " + graph.getOutDegree(10));
+        System.out.println("Grau do nó 50: " + graph.getInDegree(50));
     }
 
 }
